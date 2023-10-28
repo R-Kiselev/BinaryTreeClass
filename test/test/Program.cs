2708a34ed60bab7c3884092a7a2132e79672d81b
Roman Kiselev <sputnavik@gmail.com>
2023-10-29 00:15:57 +0300

    initial commit

---
+++ test/test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var values = new object[] { 1, 2, "3", 4, 5 };
+            var tmpValue = values.Cast<int>();
+            foreach (var str in tmpValue)
+            { Console.Write(str); }
+        }
+       
+    }
+}
